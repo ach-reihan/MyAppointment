cc835b520cc8229405f34bf82704a440e925a975
Achmad Reihan Alfaiz <reihan3000@gmail.com>
2026-05-26 11:40:01 +0800

    feat: enhance AppointmentFactory with predefined complaints and faker usage

--- database/factories/AppointmentFactory.php
+++ database/factories/AppointmentFactory.php
@@ -20,12 +20,24 @@ class AppointmentFactory extends Factory
      */
     public function definition(): array
     {
+        $faker = fake('id_ID');
+        $complaints = [
+            'Demam sejak dua hari terakhir',
+            'Batuk dan pilek yang tidak kunjung reda',
+            'Sakit kepala disertai pusing',
+            'Nyeri pada perut bagian bawah',
+            'Badan terasa lemas dan tidak bertenaga',
+            'Nyeri gigi saat mengunyah makanan',
+            'Sesak napas saat aktivitas ringan',
+            'Mual setelah makan',
+        ];
+
         return [
             'patient_id' => Patient::factory(),
             'doctor_id' => Doctor::factory(),
             'clinic_id' => Clinic::factory(),
-            'appointment_datetime' => fake()->dateTimeBetween('-1 month', '+1 month'),
-            'complaint' => fake()->sentence(6),
+            'appointment_datetime' => $faker->dateTimeBetween('-1 month', '+1 month'),
+            'complaint' => $faker->randomElement($complaints),
             'status' => 'pending',
         ];
     }
