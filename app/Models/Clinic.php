1bc9e07d35b7c62c150785bc5b3b53ce6ccb87ca
Achmad Reihan Alfaiz <reihan3000@gmail.com>
2026-05-26 11:16:16 +0800

    refactor: add HasFactory trait and update docblock in Clinic model

--- app/Models/Clinic.php
+++ app/Models/Clinic.php
@@ -2,9 +2,14 @@
 
 namespace App\Models;
 
+use Database\Factories\ClinicFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/** @use HasFactory<ClinicFactory> */
 class Clinic extends Model
 {
+    use HasFactory;
+
     protected $guarded = [];
 }
