85f95b473ea9e34e617bcf2d360b245a8420d33c
Achmad Reihan Alfaiz <reihan3000@gmail.com>
2026-05-26 11:13:00 +0800

    refactor: update UserSeeder to use factories for user creation

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
 use Faker\Factory as Faker;
 
 class UserSeeder extends Seeder
@@ -16,31 +15,25 @@ class UserSeeder extends Seeder
     {
         $faker = Faker::create('id_ID');
 
-        User::create([
+        User::factory()->admin()->create([
             'name' => 'Administrator',
             'username' => 'admin',
             'email' => 'admin@hospital.com',
-            'password' => Hash::make('password'),
-            'role' => 'admin',
         ]);
 
         for ($i = 0; $i < 5; $i++) {
-            User::create([
+            User::factory()->doctor()->create([
                 'name' => $faker->name,
                 'username' => 'doctor' . ($i + 1),
                 'email' => 'doctor' . ($i + 1) . '@hospital.com',
-                'password' => Hash::make('password'),
-                'role' => 'doctor',
             ]);
         }
 
         for ($i = 1; $i <= 20; $i++) {
-            User::create([
+            User::factory()->patient()->create([
                 'name' => $faker->name,
                 'username' => 'patient' . $i,
                 'email' => 'patient' . $i . '@gmail.com',
-                'password' => Hash::make('password'),
-                'role' => 'patient',
             ]);
         }
     }
