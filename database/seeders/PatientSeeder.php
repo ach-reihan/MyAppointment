9ad4fe46909ce0d61bc19ef7206ee53818ee8e72
Achmad Reihan Alfaiz <reihan3000@gmail.com>
2026-05-26 11:18:18 +0800

    refactor: simplify PatientSeeder by using factory for patient creation

--- database/seeders/PatientSeeder.php
+++ database/seeders/PatientSeeder.php
@@ -3,9 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Patient;
-use App\Models\User;
 use Illuminate\Database\Seeder;
-use Faker\Factory as Faker;
 
 class PatientSeeder extends Seeder
 {
@@ -14,17 +12,8 @@ class PatientSeeder extends Seeder
      */
     public function run(): void
     {
-        $faker = Faker::create('id_ID');
-        $patientUsers = User::query()->where('role', 'patient')->orderBy('id')->get();
-
-        foreach ($patientUsers as $user) {
-            Patient::create([
-                'user_id' => $user->id,
-                'full_name' => $user->name,
-                'address' => $faker->address,
-                'date_of_birth' => $faker->date('Y-m-d', '2010-01-01'),
-                'phone_number' => '08' . $faker->numerify('##########'),
-            ]);
+        for ($i = 0; $i < 20; $i++) {
+            Patient::factory()->create();
         }
     }
 }
