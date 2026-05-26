c7fa2a97b736d0557c0c22f33a103aac664ac7de
Achmad Reihan Alfaiz <reihan3000@gmail.com>
2026-05-26 11:17:17 +0800

    feat: create DoctorFactory for generating doctor test data

---
+++ database/factories/DoctorFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Doctor;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Doctor>
+ */
+class DoctorFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory()->doctor(),
+            'name' => fake()->name(),
+            'specialization' => 'Spesialis ' . fake()->randomElement(['Umum', 'Gigi', 'Anak', 'Penyakit Dalam', 'Mata']),
+        ];
+    }
+}
