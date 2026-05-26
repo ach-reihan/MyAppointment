708144e1518cc1905527941747d04e76f74a144e
Achmad Reihan Alfaiz <reihan3000@gmail.com>
2026-05-26 11:16:51 +0800

    refactor: add HasFactory trait and update docblock in Doctor model

--- app/Models/Doctor.php
+++ app/Models/Doctor.php
@@ -2,11 +2,16 @@
 
 namespace App\Models;
 
+use Database\Factories\DoctorFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
+/** @use HasFactory<DoctorFactory> */
 class Doctor extends Model
 {
+    use HasFactory;
+
     protected $guarded = [];
 
     public function clinics(): BelongsToMany
