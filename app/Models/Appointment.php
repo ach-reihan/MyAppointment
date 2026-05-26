5d96d49c609bbf2a0bc81e5a179ad0dfbddae2c9
Achmad Reihan Alfaiz <reihan3000@gmail.com>
2026-05-26 11:14:37 +0800

    refactor: improve Appointment model by adding HasFactory trait and updating docblock

--- app/Models/Appointment.php
+++ app/Models/Appointment.php
@@ -2,10 +2,15 @@
 
 namespace App\Models;
 
+use Database\Factories\AppointmentFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/** @use HasFactory<AppointmentFactory> */
 class Appointment extends Model
 {
+    use HasFactory;
+
     protected $guarded = [];
 
     protected function casts(): array
