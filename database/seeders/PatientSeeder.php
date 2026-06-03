62aaa46cebe1cdf76e77bc91a9be442adf08ced1
Achmad Reihan Alfaiz <reihan3000@gmail.com>
2026-06-03 23:12:15 +0800

    feat: update PatientSeeder to create patients associated with users

--- database/seeders/PatientSeeder.php
+++ database/seeders/PatientSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Patient;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class PatientSeeder extends Seeder
@@ -12,8 +13,18 @@ class PatientSeeder extends Seeder
      */
     public function run(): void
     {
-        for ($i = 0; $i < 20; $i++) {
-            Patient::factory()->create();
+        $faker = \Faker\Factory::create('id_ID');
+
+        $patientUsers = User::query()->where('role', 'patient')->get();
+
+        foreach ($patientUsers as $user) {
+            Patient::factory()->create([
+                'user_id' => $user->id,
+                'name' => $user->name,
+                'address' => $faker->address(),
+                'date_of_birth' => $faker->date('Y-m-d', '2010-01-01'),
+                'phone_number' => '08' . $faker->numerify('##########'),
+            ]);
         }
     }
 }
