9bfc6d5c536bdb007c463f9fbcbc646992f625e5
Haelarz <helgalathif@gmail.com>
2026-06-05 11:14:46 +0800

    test

--- routes/doctor.php
+++ routes/doctor.php
@@ -2,18 +2,24 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('doctor.DashboardDoctor');
-})->name('DashboardDoctor');
+// Bungkus semua rute dengan prefix 'doctor' dan name 'doctor.'
+Route::prefix('doctor')->name('doctor.')->group(function () {
+    
+    // URL: /doctor/dashboard | Route Name: doctor.dashboard
+    Route::get('/dashboard', function () {
+        return view('doctor.DashboardDoctor');
+    })->name('dashboard');
 
-Route::get('/examination/index', function () {
-    return view('doctor.examination.Index');
-})->name('examination.Index'); 
+    Route::get('/examination/index', function () {
+        return view('doctor.examination.Index');
+    })->name('examination.Index'); 
 
-Route::get('/examination/{id}', function ($id) {
-    return view('doctor.examination.Show', ['id' => $id]);
-})->name('examination.Show');
+    Route::get('/examination/{id}', function ($id) {
+        return view('doctor.examination.Show', ['id' => $id]);
+    })->name('examination.Show');
 
-Route::get('/examination/{id}/detail', function ($id) {
-    return view('doctor.examination.Detail', ['id' => $id]);
-})->name('examination.Detail');
+    Route::get('/examination/{id}/detail', function ($id) {
+        return view('doctor.examination.Detail', ['id' => $id]);
+    })->name('examination.Detail');
+
+});
