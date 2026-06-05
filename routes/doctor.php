6b84aa5a602e27090be606db347fd3bf57ff1253
Dooughnut <kusotori25@gmail.com>
2026-06-05 12:08:56 +0800

    feat: update doctor routes to include '/doctor' prefix

--- routes/doctor.php
+++ routes/doctor.php
@@ -2,18 +2,26 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/doctor', function () {
     return view('doctor.DashboardDoctor');
 })->name('DashboardDoctor');
 
-Route::get('/examination/index', function () {
+Route::get('/doctor/examination/index', function () {
     return view('doctor.examination.Index');
 })->name('examination.Index'); 
 
-Route::get('/examination/{id}', function ($id) {
+Route::get('/doctor/examination/{id}', function ($id) {
     return view('doctor.examination.Show', ['id' => $id]);
 })->name('examination.Show');
 
-Route::get('/examination/{id}/detail', function ($id) {
+Route::get('/doctor/examination/{id}/detail', function ($id) {
     return view('doctor.examination.Detail', ['id' => $id]);
 })->name('examination.Detail');
+
+Route::get('/patient', function () {
+    return view('patient.DashboardPatient');
+})->name('DashboardPatient');
+
+Route::get('/patient/FormAppointment', function () {
+    return view('patient.FormAppointment');
+})->name('FormAppointment');
