7bdbec3ca6b99681f876d6acb442ee12fc398648
Achmad Reihan Alfaiz <reihan3000@gmail.com>
2026-06-05 10:26:41 +0800

    feat: enhance Clinic model with HasUlids trait and add doctors relationship

--- app/Models/Clinic.php
+++ app/Models/Clinic.php
@@ -3,13 +3,21 @@
 namespace App\Models;
 
 use Database\Factories\ClinicFactory;
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 /** @use HasFactory<ClinicFactory> */
 class Clinic extends Model
 {
-    use HasFactory;
+    use HasUlids, HasFactory;
 
     protected $guarded = [];
+
+    public function doctors(): BelongsToMany
+    {
+        return $this->belongsToMany(Doctor::class, 'clinic_doctor')
+            ->withPivot(['available_from', 'available_to']);
+    }
 }
