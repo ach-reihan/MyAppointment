54ad2a69968b96fed16ac17b6817694c65bd15a2
Achmad Reihan Alfaiz <reihan3000@gmail.com>
2026-06-07 02:55:49 +0800

    refactor: simplify MedicalRecord model by removing redundant comments and reorganizing methods

--- app/Models/MedicalRecord.php
+++ app/Models/MedicalRecord.php
@@ -3,39 +3,29 @@
 namespace App\Models;
 
 use Database\Factories\MedicalRecordFactory;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-/** @use HasFactory<MedicalRecordFactory> */
 class MedicalRecord extends Model
 {
     use HasUlids, HasFactory;
 
     protected $guarded = [];
+    
+    public function appointment(): BelongsTo
+    {
+        return $this->belongsTo(Appointment::class);
+    }
 
-    /**
-     * Relasi ke model Doctor
-     */
     public function doctor(): BelongsTo
     {
         return $this->belongsTo(Doctor::class);
     }
 
-    /**
-     * Relasi ke model Patient
-     */
     public function patient(): BelongsTo
     {
         return $this->belongsTo(Patient::class);
-    }
-
-    /**
-     * Relasi ke model Appointment
-     */
-    public function appointment(): BelongsTo
-    {
-        return $this->belongsTo(Appointment::class);
     }
 }
