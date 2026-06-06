817d2e10c0010570d5668f3c44fd9176c025c6f6
Achmad Reihan Alfaiz <reihan3000@gmail.com>
2026-06-07 02:57:20 +0800

    refactor: simplify DoctorSeeder by removing Faker and optimizing clinic attachment logic

--- database/seeders/DoctorSeeder.php
+++ database/seeders/DoctorSeeder.php
@@ -5,8 +5,8 @@ namespace Database\Seeders;
 use App\Models\Clinic;
 use App\Models\Doctor;
 use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
-use Faker\Factory as Faker;
 
 class DoctorSeeder extends Seeder
 {
@@ -15,28 +15,25 @@ class DoctorSeeder extends Seeder
      */
     public function run(): void
     {
-        $faker = Faker::create('id_ID');
         $specializations = ['Umum', 'Gigi', 'Anak', 'Penyakit Dalam', 'Mata'];
-        $clinicIds = Clinic::query()->pluck('id')->all();
+        $clinics = Clinic::all();
 
-        $doctorUsers = User::query()->where('role', 'doctor')->get();
-
-        foreach ($doctorUsers->values() as $index => $user) {
-            $specialization = $specializations[$index % count($specializations)];
-
-            $doctor = Doctor::factory()->create([
-                'user_id' => $user->id,
-                'specialization' => 'Spesialis ' . $specialization,
-            ]);
-
-            if (! empty($clinicIds)) {
-                foreach ($faker->randomElements($clinicIds, rand(1, 2)) as $clinicId) {
-                    $doctor->clinics()->attach($clinicId, [
-                        'available_from' => '08:00:00',
-                        'available_to' => '16:00:00',
-                    ]);
-                }
-            }
+        if ($clinics->isEmpty()) {
+            return;
         }
+
+        Doctor::factory()
+            ->count(5)
+            ->sequence(fn (Sequence $sequence) => [
+                'specialization' => 'Spesialis ' . $specializations[$sequence->index % count($specializations)],
+                'user_id' => User::factory()->doctor()->state([
+                    'username' => 'doctor' . ($sequence->index + 1),
+                    'email' => 'doctor' . ($sequence->index + 1) . '@hospital.com',
+                ]),
+            ])
+            ->afterCreating(function (Doctor $doctor) use ($clinics) {
+                $doctor->clinics()->attach($clinics->random(rand(1, 2)));
+            })
+            ->create();
     }
 }
