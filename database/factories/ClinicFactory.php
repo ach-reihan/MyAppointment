6ebd196816936f40c3b2765b309285f21f40566c
Achmad Reihan Alfaiz <reihan3000@gmail.com>
2026-06-07 02:56:37 +0800

    refactor: remove 'opens_at' and 'closes_at' fields from ClinicFactory for simplification

--- database/factories/ClinicFactory.php
+++ database/factories/ClinicFactory.php
@@ -21,8 +21,6 @@ class ClinicFactory extends Factory
 
         return [
             'name' => $clinicName,
-            'opens_at' => '08:00:00',
-            'closes_at' => '16:00:00',
             'description' => 'Pelayanan khusus untuk ' . $clinicName,
         ];
     }
