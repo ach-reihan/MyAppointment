976fa91c0a9cf79bb6d026180b4c72be3b4f9538
Achmad Reihan Alfaiz <reihan3000@gmail.com>
2026-06-07 02:57:49 +0800

    refactor: simplify UserSeeder by removing Faker and optimizing user creation logic

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
-use Faker\Factory as Faker;
 
 class UserSeeder extends Seeder
 {
@@ -13,28 +12,10 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        $faker = Faker::create('id_ID');
-
         User::factory()->admin()->create([
             'name' => 'Administrator',
             'username' => 'admin',
             'email' => 'admin@hospital.com',
         ]);
-
-        for ($i = 0; $i < 5; $i++) {
-            User::factory()->doctor()->create([
-                'name' => $faker->name,
-                'username' => 'doctor' . ($i + 1),
-                'email' => 'doctor' . ($i + 1) . '@hospital.com',
-            ]);
-        }
-
-        for ($i = 1; $i <= 20; $i++) {
-            User::factory()->patient()->create([
-                'name' => $faker->name,
-                'username' => 'patient' . $i,
-                'email' => 'patient' . $i . '@gmail.com',
-            ]);
-        }
     }
 }
