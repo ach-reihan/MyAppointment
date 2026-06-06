79c8ccae382da78305467221b0759ca8dc9235c1
Achmad Reihan Alfaiz <reihan3000@gmail.com>
2026-06-07 02:55:19 +0800

    feat: add relationships for patient, doctor, and clinic in Appointment model

--- app/Models/Appointment.php
+++ app/Models/Appointment.php
@@ -3,16 +3,31 @@
 namespace App\Models;
 
 use Database\Factories\AppointmentFactory;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-/** @use HasFactory<AppointmentFactory> */
 class Appointment extends Model
 {
     use HasUlids, HasFactory;
 
     protected $guarded = [];
+
+    public function patient(): BelongsTo
+    {
+        return $this->belongsTo(Patient::class);
+    }
+
+    public function doctor(): BelongsTo
+    {
+        return $this->belongsTo(Doctor::class);
+    }
+
+    public function clinic(): BelongsTo
+    {
+        return $this->belongsTo(Clinic::class);
+    }
 
     protected function casts(): array
     {
