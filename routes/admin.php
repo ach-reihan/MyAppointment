d7b0b26030387216ed9412ede08929354e46eb0d
Achmad Reihan Alfaiz <reihan3000@gmail.com>
2026-06-07 12:38:41 +0800

    feat: enhance master data routes with CRUD operations for patients and polyclinics

--- routes/admin.php
+++ routes/admin.php
@@ -11,10 +11,25 @@ Route::prefix('admin')->name('admin.')->group(function () {
     Route::get('/dashboard', [AdminDashboard::class, 'index'])->name('dashboard');
 
     Route::prefix('master-data')->name('master-data.')->group(function () {
-        Route::get('/users',       [MasterDataController::class, 'users'])->name('users');
-        Route::get('/doctors',     [MasterDataController::class, 'doctors'])->name('doctors');
-        Route::get('/patients',    [MasterDataController::class, 'patients'])->name('patients');
-        Route::get('/polyclinics', [MasterDataController::class, 'polyclinics'])->name('polyclinics');
+        Route::get('/users',          [MasterDataController::class, 'users'])->name('users');
+        Route::post('/users',         [MasterDataController::class, 'storeUser'])->name('users.store');
+        Route::put('/users/{id}',     [MasterDataController::class, 'updateUser'])->name('users.update');
+        Route::delete('/users/{id}',  [MasterDataController::class, 'destroyUser'])->name('users.destroy');
+
+        Route::get('/doctors',        [MasterDataController::class, 'doctors'])->name('doctors');
+        Route::post('/doctors',       [MasterDataController::class, 'storeDoctor'])->name('doctors.store');
+        Route::put('/doctors/{id}',   [MasterDataController::class, 'updateDoctor'])->name('doctors.update');
+        Route::delete('/doctors/{id}', [MasterDataController::class, 'destroyDoctor'])->name('doctors.destroy');
+
+        Route::get('/patients',       [MasterDataController::class, 'patients'])->name('patients');
+        Route::post('/patients',      [MasterDataController::class, 'storePatient'])->name('patients.store');
+        Route::put('/patients/{id}',  [MasterDataController::class, 'updatePatient'])->name('patients.update');
+        Route::delete('/patients/{id}', [MasterDataController::class, 'destroyPatient'])->name('patients.destroy');
+
+        Route::get('/polyclinics',     [MasterDataController::class, 'polyclinics'])->name('polyclinics');
+        Route::post('/polyclinics',    [MasterDataController::class, 'storePolyclinic'])->name('polyclinics.store');
+        Route::put('/polyclinics/{id}', [MasterDataController::class, 'updatePolyclinic'])->name('polyclinics.update');
+        Route::delete('/polyclinics/{id}', [MasterDataController::class, 'destroyPolyclinic'])->name('polyclinics.destroy');
     });
 
     Route::prefix('operational')->name('operational.')->group(function () {
