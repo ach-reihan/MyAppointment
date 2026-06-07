909d602150497d916b22b8ba83d95afebcbd7de1
Dooughnut <kusotori25@gmail.com>
2026-06-07 20:06:40 +0800

    feat: implement PatientController for managing patient-related functionalities

---
+++ app/Http/Controllers/Patient/PatientController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\Patient;
+
+use Illuminate\Http\Request;
+use App\Services\Patient\PatientServices;
+class PatientController
+{
+    protected $patientService;
+    public function __construct(PatientServices $patientService)
+    {
+        $this->patientService = $patientService;
+    }
+
+    public function dashboard()
+    {
+        $patient = $this->patientService->getPatientProfile();
+        $histories = $this->patientService->getPatientHistory();
+        $upcomingAppointments = $this->patientService->getUpcomingAppointments();
+
+        return view('patient.DashboardPatient', compact('patient', 'histories', 'upcomingAppointments'));
+    }
+
+    public function showAppointmentDetail($id)
+    {
+        $appointment = $this->patientService->getAppointmentDetail($id);
+        
+        if (!$appointment) {
+            abort(404, 'Detail Janji Temu tidak ditemukan');
+        }
+        
+        return view('patient.AppointmentDetail', compact('appointment'));
+    }
+
+    public function medicalHistory()
+    {
+        $histories = $this->patientService->getPatientHistory();
+        return view('patient.MedicalHistoryList', compact('histories')); 
+    }
+
+    public function medicalHistoryDetail($id)
+    {
+        $history = $this->patientService->getHistoryDetail($id);
+        $patient = $this->patientService->getPatientProfile();
+
+        if (!$history) {
+            abort(404, 'Riwayat tidak ditemukan');
+        }
+
+        return view('patient.MedicalHistoryDetail', compact('history', 'patient'));
+    }
+}
