ad602499e25c3733acd34b5a51d91081be58c699
Achmad Reihan Alfaiz <reihan3000@gmail.com>
2026-06-08 23:32:52 +0800

    feat: update DoctorFactory to include a wider range of specializations

--- database/factories/DoctorFactory.php
+++ database/factories/DoctorFactory.php
@@ -22,7 +22,18 @@ class DoctorFactory extends Factory
 
         return [
             'user_id' => User::factory()->doctor(),
-            'specialization' => 'Spesialis ' . $faker->randomElement(['Umum', 'Gigi', 'Anak', 'Penyakit Dalam', 'Mata']),
+            'specialization' => 'Spesialis ' . $faker->randomElement([
+                'Spesialis Penyakit Dalam',
+                'Spesialis Bedah Umum',
+                'Spesialis Anak',
+                'Spesialis Obstetri dan Ginekologi',
+                'Spesialis Saraf',
+                'Spesialis Jantung dan Pembuluh Darah',
+                'Spesialis Mata',
+                'Spesialis THT',
+                'Spesialis Kulit dan Kelamin',
+                'Dokter Umum'
+            ]),
         ];
     }
 }
