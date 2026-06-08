216e5da0231ef7a59590146a5ba9bf6c0fd53182
Achmad Reihan Alfaiz <reihan3000@gmail.com>
2026-06-08 15:17:02 +0800

    fix: add authentication and role middleware to doctor routes

--- routes/doctor.php
+++ routes/doctor.php
@@ -3,7 +3,7 @@ use App\Http\Controllers\Doctor\ExaminationController;
 use App\Http\Controllers\Doctor\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('doctor')->group(function () {
+Route::prefix('doctor')->middleware(['auth', 'role:doctor'])->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('DashboardDoctor');
     
     Route::get('/examination', [ExaminationController::class, 'index'])->name('examination.Index');
